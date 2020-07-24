dce2fd43bc90e0ae779322661f261eb428cafee9
Cam C <ckc.cameron@gmail.com>
2020-07-24 09:59:37 -0600

    Add dish total calorie count to dish show spec

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,24 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "dishes show page" do
+  before :each do
+    @chef = Chef.create(name: "Mario Luigi")
+
+    @spaghetti = Dish.create(name: "Spaghetti", description: "molto delizioso", chef_id: @chef.id)
+
+    @noodles = Ingredient.create(name: "Angel Hair Noodles", calories: 250)
+    @sauce = Ingredient.create(name: "Basilico Sauce", calories: 300)
+    @sausage = Ingredient.create(name: "Italian Sausage", calories: 400)
+
+    DishIngredient.create(dish: @spaghetti, ingredient: @noodles)
+    DishIngredient.create(dish: @spaghetti, ingredient: @sauce)
+    DishIngredient.create(dish: @spaghetti, ingredient: @sausage)
+  end
   it "displays a list of ingredients and the chef name for a dish" do
-    chef = Chef.create(name: "Mario Luigi")
+    visit "/dish/#{@spaghetti.id}"
 
-    spaghetti = Dish.create(name: "Spaghetti", description: "molto delizioso", chef_id: chef.id)
+    expect(page).to have_content(@noodles.name)
+    expect(page).to have_content(@sauce.name)
+    expect(page).to have_content(@sausage.name)
+    expect(page).to have_content(@chef.name)
+  end
 
-    noodles = Ingredient.create(name: "Angel Hair Noodles", calories: 250)
-    sauce = Ingredient.create(name: "Basilico Sauce", calories: 300)
-    sausage = Ingredient.create(name: "Italian Sausage", calories: 400)
+  it "displays total sum of calories for a dish" do
+    visit "/dish/#{@spaghetti.id}"
 
-    DishIngredient.create(dish: spaghetti, ingredient: noodles)
-    DishIngredient.create(dish: spaghetti, ingredient: sauce)
-    DishIngredient.create(dish: spaghetti, ingredient: sausage)
-
-    visit "/dish/#{spaghetti.id}"
-    
-    expect(page).to have_content(noodles.name)
-    expect(page).to have_content(sauce.name)
-    expect(page).to have_content(sausage.name)
-    expect(page).to have_content(chef.name)
+    expect(page).to have_content("Total Calories: 950")
   end
 end
