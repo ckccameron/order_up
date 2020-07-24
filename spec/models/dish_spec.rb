33ce50bb0afa4108f8807bc39b0359421ce40e40
Cam C <ckc.cameron@gmail.com>
2020-07-24 10:02:20 -0600

    Write total calories method spec for dish model

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,21 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+  describe "methods" do
+    it "#total_calories" do
+      chef = Chef.create(name: "Mario Luigi")
+
+      spaghetti = Dish.create(name: "Spaghetti", description: "molto delizioso", chef_id: chef.id)
+
+      noodles = Ingredient.create(name: "Angel Hair Noodles", calories: 250)
+      sauce = Ingredient.create(name: "Basilico Sauce", calories: 300)
+      sausage = Ingredient.create(name: "Italian Sausage", calories: 400)
+
+      DishIngredient.create(dish: spaghetti, ingredient: noodles)
+      DishIngredient.create(dish: spaghetti, ingredient: sauce)
+      DishIngredient.create(dish: spaghetti, ingredient: sausage)
+
+      expect(spaghetti.total_calories).to eq(950)
+    end
+  end
 end
