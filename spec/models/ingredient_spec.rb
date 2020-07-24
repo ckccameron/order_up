47a1a7ebda839be52090a02e61093b59334c8dec
Cam C <ckc.cameron@gmail.com>
2020-07-24 09:28:33 -0600

    Create Ingredient model

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Ingredient do
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :calories}
+  end
 
   describe "relationships" do
     it {should have_many :dish_ingredients}
